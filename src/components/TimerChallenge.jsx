d3c0b7e28f54a6cf8b367c1cadb4fe609bd56b4c
kushanz <kushanshanuka@yahoo.com>
2025-12-22 16:05:49 +0530

    timer challenge

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+export default function TimerChallenge({title, targetTime}) {
+
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [timerExpired, setTimerExpired] = useState(false)
+
+
+    function handleStart() {
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000);
+
+        setTimerStarted(true)
+    }
+
+    return <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You Lost</p> }
+        <p className="challenge-time">
+            {targetTime} second{targetTime>1? 's' : ''}
+        </p>
+        <p>
+            <button onClick={handleStart}>{timerStarted ? 'stop' : 'start'} Challenge</button>
+        </p>
+        <p className="active">
+            {timerStarted ? 'Time is running' : 'Timer inactive'}
+        </p>
+    </section>
+}
