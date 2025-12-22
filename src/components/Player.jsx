7299ef9b9bcf47ca108d76ff12a7bc07bac68c47
kushanz <kushanshanuka@yahoo.com>
2025-12-22 12:02:09 +0530

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,16 @@
+export { useState } from 'react';
+export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  onChange(event) {
+    setPlayerName(event.target.value)
+  }
+  return (
+    <section id="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+}
